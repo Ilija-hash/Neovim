d60bbb1147635e86bc792408277be474052988fa
Ilija <ilijastevanovic005@gmail.com>
2026-04-13 18:36:46 +0200

    disable autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "okuuva/auto-save.nvim",
-    event = { "InsertLeave", "TextChanged" },
-    opts = {
-      enabled = true,
-      debounce_delay = 1000, -- 1s posle promene
-      trigger_events = {
-        immediate_save = { "BufLeave", "FocusLost" },
-        defer_save = { "InsertLeave", "TextChanged" },
-      },
-    },
-  },
+  --   {
+  --     "okuuva/auto-save.nvim",
+  --     event = { "InsertLeave", "TextChanged" },
+  --     opts = {
+  --       enabled = true,
+  --       debounce_delay = 1000, -- 1s posle promene
+  --       trigger_events = {
+  --         immediate_save = { "BufLeave", "FocusLost" },
+  --         defer_save = { "InsertLeave", "TextChanged" },
+  --       },
+  --     },
+  --   },
 }
